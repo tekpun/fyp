0ada48849e03ed9ca63807ba06b0308537257036
tekpun <94120673+tekpun@users.noreply.github.com>
2024-03-05 17:13:40 +0000

    Add files via upload

--- lightsensor.py
+++ lightsensor.py
@@ -1,21 +1,37 @@
+
 import RPi.GPIO as GPIO
 import time
 
+
 GPIO.setmode(GPIO.BOARD)
 
-resistorPin = 7
+pin_to_circuit = 7
 
-while True:
-    GPIO.setup(resistorPin, GPIO.OUT)
-    GPIO.output(resistorPin, GPIO.LOW)
+
+def rc_time(pin_to_circuit):
+    count = 0
+
+    # Output on the pin for
+    GPIO.setup(pin_to_circuit, GPIO.OUT)
+    GPIO.output(pin_to_circuit, GPIO.LOW)
     time.sleep(0.1)
 
-    GPIO.setup(resistorPin, GPIO.IN)
-    currentTime = time.time()
-    diff = 0
+    # Change the pin back to input
+    GPIO.setup(pin_to_circuit, GPIO.IN)
 
-    while (GPIO.input(resistorPin) == GPIO.LOW):
-        diff = time.time() - currentTime
+    # Count until the pin goes high
+    while (GPIO.input(pin_to_circuit) == GPIO.LOW):
+        count += 1
 
-    print(diff * 1000)
-    time.sleep(1)
+    return count
+
+
+# Catch when script is interrupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+        print(rc_time(pin_to_circuit))
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
